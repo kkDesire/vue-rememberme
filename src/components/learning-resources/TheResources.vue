fa3fd88eed20adb019f0c0ab2b969a0edf1bd2e7
Desire KOUASSI <kouamdesire90@gmail.com>
2022-03-03 15:26:07 +0000

    Add Deleting item (editing the files LearningResource.vue, StoredResources.vue and TheResources.vue

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -47,6 +47,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource,
     };
   },
   computed: {
@@ -70,6 +71,12 @@ export default {
       };
       this.storedResources.unshift(newResource);
       this.selectedTab = "stored-resources";
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
     },
   },
 };
